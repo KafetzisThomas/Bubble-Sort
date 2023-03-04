1ebefbf39778276d371d4bcf5c777ed7064644a8
KafetzisThomas <tomkafetzis06@gmail.com>
2023-03-04 18:44:56 +0200

    Add comment

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,7 +7,7 @@
 def bubble_sort(list):
   for i in range(len(list)):
     for j in range(i+1, len(list)):
-      if list[i] > list[j]: 
+      if list[i] > list[j]: # Otherwise, use '<' to print a reversed list
         temp = list[i]
         list[i] = list[j]
         list[j] = temp
