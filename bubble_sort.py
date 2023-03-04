0d8fcf4abcdb6003ca842339a0e8dcd1a91ddd33
KafetzisThomas <tomkafetzis06@gmail.com>
2023-03-04 18:37:53 +0200

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+# Project Title: Bubble-Sort (https://github.com/KafetzisThomas/Bubble-Sort)
+# Author / Project Owner: KafetzisThomas - (https://github.com/KafetzisThomas)
+
+def bubble_sort(list):
+  for i in range(len(list)):
+    for j in range(i+1, len(list)):
+      if list[i] > list[j]: 
+        temp = list[i]
+        list[i] = list[j]
+        list[j] = temp
+
+  print(list)
+
+bubble_sort([19, 13, 6, 2, 18, 8])
